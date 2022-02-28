9db5ae5a27b584ea0094aa588e0d25b64d106637
sooriowl <sooriowl@gmail.com>
2022-02-28 20:38:55 +0900

    첫 업로드

---
+++ src/kh_test/test01.java
@@ -0,0 +1,13 @@
+package kh_test;
+
+public class test01 {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("안녕하세요");
+		System.out.println("깃허브 테스트");
+		System.out.println("입니다");
+	}
+
+}
