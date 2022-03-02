32a4419f935af424fb930262f69c74d07850bfd3
julia-serif <juliainserif@gmail.com>
2022-03-02 11:35:31 +0900

    sql test

---
+++ src/init.sql
@@ -0,0 +1,13 @@
+create table member(
+    mem_id number(10) primary key,
+    mem_password varchar(100) not null,
+    mem_name varchar(20) not null,
+    mem_contact varchar(20),
+    mem_addr varchar(100),
+    mem_mileage number(10)
+);
+
+create view mem_info
+as
+select mem_id, mem_name, mem_contact, mem_addr, mem_mileage
+from member;
