4e85148b72ce62a38883d85666ab0ae0ab812c82
5-gj <ogj5555@naver.com>
2022-03-02 10:34:05 +0900

    ss

--- src/gittest1/gittest.java
+++ src/gittest1/gittest.java
@@ -4,7 +4,7 @@ public class gittest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("test 5gj");
+System.out.println("test 5gj111");
 	}
 
 }
