5c7be8f324ee4b9cb0ef0bf91adb53b6cd42ffa2
julia-serif <juliainserif@gmail.com>
2022-03-08 17:22:31 +0900

    이름으로 아이디 찾기 구현

--- team3/init/MemberInfo.java
+++ team3/init/MemberInfo.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class MemberInfo extends JFrame {
 
@@ -30,12 +32,29 @@ public class MemberInfo extends JFrame {
 	 * Create the frame.
 	 */
 	public MemberInfo() {
+		setTitle("로그인됨");
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel = new JLabel("안녕하세요.");
+		panel.add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.SOUTH);
+		
+		JButton btnNewButton_1 = new JButton("회원정보 확인");
+		panel_1.add(btnNewButton_1);
+		
+		JButton btnNewButton = new JButton("회원정보 수정");
+		panel_1.add(btnNewButton);
 		setVisible(true);
 	}
 
