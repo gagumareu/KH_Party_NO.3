6476b9e5f89c37f5b3f7fe262a2e30d14fee5ad1
julia-serif <juliainserif@gmail.com>
2022-03-12 14:51:38 +0900

    setDefaultCloseOperation

--- src/team3/init/MyPage.java
+++ src/team3/init/MyPage.java
@@ -28,82 +28,22 @@ public class MyPage extends JFrame {
 	private JPanel contentPane;
 
 	/**
-	 * Launch the application.
+	 * Application launched by other application - Init
+	 * 기본 생성자는 사용되지 않음.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MyPage frame = new MyPage(id);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
 	 */
-/*	public MyPage() {
-		setTitle("마이 페이지");
-		
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 707, 607);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.CENTER);
-		
-		JPanel panel_1 = new JPanel();
-		panel.add(panel_1);
-		
-		JLabel jl_greet1 = new JLabel("안녕하세요.");
-		panel_1.add(jl_greet1);
-		
-		JPanel panel_2 = new JPanel();
-		panel.add(panel_2);
-		
-		JLabel jl_greet2 = new JLabel("마이 페이지에 온 것을 환영합니다.");
-		panel_2.add(jl_greet2);
-		
-		JPanel panelBtn = new JPanel();
-		contentPane.add(panelBtn, BorderLayout.SOUTH);
-		
-		JButton btnCheck = new JButton("회원정보 확인");
-		btnCheck.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				new MemberInfoCheck();
-			}
-		});
-		panelBtn.add(btnCheck);
-		
-		JButton btnEdit = new JButton("회원정보 수정");
-		btnEdit.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				new MemberInfoEdit();
-			}
-		});
-		panelBtn.add(btnEdit);
-		setVisible(true);
-	}
-	*/
-	
-	
-	
+
 	public MyPage(String id) {
 		
 		
 		setTitle("마이 페이지");
-		
-		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		contentPane = new JPanel();
 		setContentPane(contentPane);
+		
 		JPanel container = new JPanel();
 		
 		JPanel container1 = new JPanel();
@@ -128,7 +68,7 @@ public class MyPage extends JFrame {
 		
 		JLabel jl_greet1 = new JLabel("안녕하세요, " + id + "님.");
 		
-		JLabel jl_greet2 = new JLabel("로그인 하셨습니다..");
+		JLabel jl_greet2 = new JLabel("마이 페이지에 오신 것을 환영합니다.");
 		
 		jl_greet1.setFont(new Font("휴먼편지체",Font.BOLD, 30));
 		jl_greet2.setFont(new Font("휴먼편지체",Font.BOLD, 30));
